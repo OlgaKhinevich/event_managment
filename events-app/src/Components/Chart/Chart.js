ba1bab65c95fc1d35b2ed51bc5b03b5f085ba199
bondcoder97 <andruha3097@gmail.com>
2020-05-26 01:02:33 +0300

    Chart Show

--- events-app/src/Components/Chart/Chart.js
+++ events-app/src/Components/Chart/Chart.js
@@ -2,21 +2,14 @@ import React, { Component } from 'react';
 import GanttChart from "react-google-charts";
 import socket from "../../connection"; 
 
+
 export default class Chart extends Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-      events: [
-        '2014Spring',
-      'Spring 2014',
-      'spring',
-      new Date(2014, 2, 22),
-      new Date(2014, 5, 20),
-      null,
-      100,
-      null,]  
+      events:[]
     };
   }
 
@@ -29,16 +22,18 @@ export default class Chart extends Component {
       try {
           if(!chartInfo) throw new Error("Ошибка во время получения информации для графика!");
           for (let i=0; i<chartInfo.length;i++) {
-            chartInfo[i].date = this.presentDate(chartInfo[i].date);
-            chartInfo[i].prepDate = this.presentDate(chartInfo[i].prepDate);
+            chartInfo[i].date = this.presentDate(chartInfo[i][2]);
+            chartInfo[i].prepDate = this.presentDate(chartInfo[i][3]);
           }
+     
           const mapResult = chartInfo.map((item, i)=>{
             return [item["id"] = i, item["name"], item["prepDate"], item["date"], item["percent"]];
-            })
-            this.setState({
+          });
+
+    
+          this.setState({
             events: mapResult
-            })
-            console.log(this.state.events)
+          });
       }
       catch(err) {
         console.log(err);
@@ -48,14 +43,9 @@ export default class Chart extends Component {
   }
 
   presentDate = (serverDateString) => {
-    let serverDate = new Date(serverDateString);
-    let day1 = serverDate.getDate();
-    let day =  this.addZeros(day1);
-    let month1 = serverDate.getMonth()+1;
-    let month = this.addZeros(month1);
-    let year = serverDate.getFullYear();
-    let date = `${day}.${month}.${year}`;
-    return date; 
+    const [day, month, year] = serverDateString.split(".");
+    return new Date(year, month, day);
+
   }
 
   addZeros = (number) => {
@@ -70,7 +60,7 @@ export default class Chart extends Component {
     return (
       <div className="chart-content">
       <h2>График мероприятий</h2>
-        <GanttChart
+      {<GanttChart
           width={'100%'}
           height={'400px'}
           chartType="Gantt"
@@ -78,23 +68,21 @@ export default class Chart extends Component {
           data={[
             [
               { type: 'string', label: 'Task ID' },
-      { type: 'string', label: 'Task Name' },
-      { type: 'string', label: 'Resource' },
-      { type: 'date', label: 'Start Date' },
-      { type: 'date', label: 'End Date' },
-      { type: 'number', label: 'Duration' },
-      { type: 'number', label: 'Percent Complete' },
-      { type: 'string', label: 'Dependencies' },
+              { type: 'string', label: 'Task Name' },
+              { type: 'string', label: 'Resource' },
+              { type: 'date', label: 'Start Date' },
+              { type: 'date', label: 'End Date' },
+              { type: 'number', label: 'Duration' },
+              { type: 'number', label: 'Percent Complete' },
+              { type: 'string', label: 'Dependencies' },
     
             ],
 
            
-         
-           
-           /*...this.state.events.map((event) => {
-            let mass = [event[0], event[1], event[2], event[3], this.daysToMilliseconds(1), event[4], null];
-            return mass;
-            }) */
+          ...this.state.events.map((event) => {
+              let mass = [event[0], event[1], "Resource" ,event[2], event[3], null, event[4]||0, null];
+              return mass;
+            }) 
 
           ]}
           options={{
@@ -103,9 +91,11 @@ export default class Chart extends Component {
               trackHeight: 30,
             },
           }}
-          //rootProps={{ 'data-testid': '2' }}
+         
         />
+  }
       </div>
+      
     );
   }
 }
